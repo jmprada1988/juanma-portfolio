75c6f6c928febe514572b7808f605783e73a4dcc
jmprada1988 <juanma.prada88@gmail.com>
2025-02-21 19:25:16 -0500

    adds sitemap files

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://juanmanuelprada.com.com',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+}
